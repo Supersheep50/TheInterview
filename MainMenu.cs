92ad0dd190a1bfaafee3e9f78cf4b79c1dce8c36
jonsmacbookair <jonathan.morrisseyto@gmail.com>
2025-07-09 14:03:11 +0100

    Initial commit

---
+++ MainMenu.cs
@@ -0,0 +1,24 @@
+
+
+
+namespace TextRPG
+{
+
+    class MainMenu
+    {
+        public static void Menu()
+        {
+            Console.WriteLine("------------");
+            Console.WriteLine("THE INTERVIEW");
+            Console.WriteLine("------------");
+            Console.WriteLine("1. New Game");
+            Console.WriteLine("2. Save Game");
+            Console.WriteLine("3. Load Game");
+            Console.WriteLine("4. About");
+            Console.WriteLine("5. Exit");
+            Console.WriteLine("------------");
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+
+namespace TextRPG {
+    
+
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Welcome to the first line of the game!");
+
+        Console.WriteLine("Welcome to the first line of the game!");
+
+        MainMenu.Menu();
+
+
+
+    }
+}
+}
