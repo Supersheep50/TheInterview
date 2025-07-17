f9ade6d0cc051daf420bea36e5c27a9ab7e9904d
jonsmacbookair <jonathan.morrisseyto@gmail.com>
2025-07-17 12:04:27 +0100

    Added basic Blazor placeholder

--- obj/Debug/net9.0/TextRPG.AssemblyInfo.cs
+++ obj/Debug/net9.0/TextRPG.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TextRPG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63d0698a3ec485cf05671104c9432dee333a0363")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dd47c6449b4ca14aa8369ca417d8ebb1a831b3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TextRPG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TextRPG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
